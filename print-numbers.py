0dbe7c575b60c81df19527421ab0434dac3e0efd
ashishkashingari <ashishkashingari@gmail.com>
2026-03-27 15:30:16 +0530

    Added first Python program

---
+++ print-numbers.py
@@ -0,0 +1,9 @@
+#print number from 1 to 30 using for loop.
+for i in range(1,31): # used 31 because last value is not executed only execute till 30.
+ print(i)
+
+ #using while loop.
+i=1
+while i<=30:
+    print(i)
+    i+=1    
