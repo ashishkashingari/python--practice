79e59d1ef5dd33b5be8872f250079bbe5544814e
ashishkashingari <ashishkashingari@gmail.com>
2026-03-30 13:58:15 +0530

    Added even_odd and sum programs

---
+++ even_odd.py
@@ -0,0 +1,16 @@
+#Print even and odd number using for loop
+for i in range(1,31):
+    if i%2==0:
+        print("Even")
+    else:
+        print("Odd")
+
+# using While loop
+i=1
+while i<=30:
+        if i%2==0:
+          print("Even")
+else:
+        print("Odd")
+        i+=1
+
---
+++ sum_numbers.py
@@ -0,0 +1,13 @@
+#sum of numbers from 1 to 50.(for loop)
+sum=0
+for i in range(1,51):
+   sum+=i
+print("The sum from 1 to 50 is:",sum)
+
+# using while loop
+total=0
+i=1
+while i<=50:
+   total+=i
+   i+=1
+print("The addition from 1 to 50 number is:",total)
