5f9219f9e76291e007a221955fedf037f8d0588b
ashishkashingari <ashishkashingari@gmail.com>
2026-04-25 21:40:21 +0530

    "added more practice questions in level-2 file"

--- dictionaries/dict_level-2.py
+++ dictionaries/dict_level-2.py
@@ -1,19 +1,45 @@
-#Loop through dictionary
+# #Loop through dictionary
 student = { #dict name
-    "name":"Ashi", #keys of dict
-    "age" : 20,
-}
+     "name":"Ashi", #keys of dict
+     "age" : 20,
+ }
 for key in student: #loop start
-    student[key]
+     student[key]
 print(student[key])#print studentkeys
 
-#Count frequency (input hello)
+# #Count frequency 
 dict_freq={}#empty dict
 s=input("Enter the string: ")#taking input from user
 for char in s:#loop
- if char in dict_freq:#condition
-    dict_freq[char]+=1
- else:
-    dict_freq[char]=1
+  if char in dict_freq:#condition
+     dict_freq[char]+=1
+  else:
+     dict_freq[char]=1
 for key in dict_freq:#for print the frequency
- print(key, ":" ,dict_freq[key])
+  print(key, ":" ,dict_freq[key])
+
+ #Sum of values
+marks={ #dict
+   "English":90,
+   "Maths":80,
+   "Science":85,
+ }
+total=0#variable to store sum of values
+for num in marks.values():#loop  which go thorugh all key value
+ total+=num #add values
+print("Sum of all values are:",total)#print the sum of key values
+
+#Find max  value in dict
+max_val=0
+for key in marks:#loop 
+ marks[key]
+ if marks[key]>max_val:
+  max_val=marks[key]
+print("max value is:",max_val)
+
+#Search key in dict
+key=input("Enter key: ")
+if key in student:
+ print("Found")
+else:
+  print("Not found")
